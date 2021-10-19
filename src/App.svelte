0c5e1de8cf926b6defdbd9e3d0a219e8a3d8a8b2
Greg Marine <gregmarine@hey.com>
2021-10-19 17:57:31 -0400

    Created the Navbar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script>
 	import Router from 'svelte-spa-router';
-	import {link} from 'svelte-spa-router';
 	import routes from './routes';
+
+	import Navbar from './components/Navbar.svelte';
 </script>
 
 <style global>
@@ -14,42 +15,8 @@
 	}
 </style>
 
-<div class="flex flex-col items-center flex-grow h-full w-full overflow-auto text-center mb-20">
+<div class="flex flex-col items-center flex-grow h-full w-full overflow-auto text-center mb-20 lg:mb-0">
 	<Router {routes} restoreScrollState={true} />
 </div>
 
-<div class="navbar flex-none mb-2 shadow-lg bg-neutral text-neutral-content rounded-box w-full fixed bottom-0 top-auto lg:bottom-auto lg:top-0">
-	<div class="px-2 mx-2 navbar-start">
-		<span class="text-lg font-bold">
-			Greg Marine
-		</span>
-	</div> 
-	<div class="hidden px-2 mx-2 navbar-center md:flex">
-		<div class="flex items-stretch">
-			<a href="/" class="btn btn-ghost btn-sm rounded-btn" use:link>
-				Home
-			</a>
-			<a href="/zen" class="btn btn-ghost btn-sm rounded-btn" use:link>
-				Zen
-			</a>
-			<a href="/dadjokes" class="btn btn-ghost btn-sm rounded-btn" use:link>
-				Dad Jokes
-			</a>
-			<a href="/blog" class="btn btn-ghost btn-sm rounded-btn" use:link>
-				Blog
-			</a>
-		</div>
-	</div> 
-	<div class="navbar-end">
-		<button class="btn btn-square btn-ghost">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">     
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>                     
-			</svg>
-		</button> 
-		<button class="btn btn-square btn-ghost">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">             
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>             
-			</svg>
-		</button>
-	</div>
-</div>
+<Navbar/>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,70 @@
+<script>
+  import { fade } from 'svelte/transition';
+	import { link } from 'svelte-spa-router';
+
+  let showMenu = false;
+
+  const toggleMenu = () => {
+    showMenu = !showMenu;
+  }
+</script>
+
+<!-- Menu -->
+{#if showMenu}
+<div class="py-4 fixed right-0 bottom-16" transition:fade>
+  <ul class="menu py-3 shadow-lg bg-base-100 rounded-box">
+    <li>
+      <a href="/" on:click={toggleMenu} use:link>
+				Home
+			</a>
+    </li> 
+    <li>
+      <a href="/zen" on:click={toggleMenu} use:link>
+				Zen
+			</a>
+    </li> 
+    <li>
+      <a href="/dadjokes" on:click={toggleMenu} use:link>
+				Dad Jokes
+			</a>
+    </li> 
+    <li>
+      <a href="/blog" on:click={toggleMenu} use:link>
+				Blog
+			</a>
+    </li>
+  </ul>
+</div>
+{/if}
+
+<!-- Navbar -->
+<div class="navbar flex-none mb-2 shadow-lg bg-neutral text-neutral-content rounded-box w-full fixed bottom-0 top-auto lg:bottom-auto lg:top-0">
+	<div class="px-2 mx-2 navbar-start">
+		<span class="text-lg font-bold">
+			Greg Marine
+		</span>
+	</div> 
+	<div class="hidden px-2 mx-2 navbar-center md:flex">
+		<div class="flex items-stretch">
+			<a href="/" class="btn btn-ghost btn-sm rounded-btn" use:link>
+				Home
+			</a>
+			<a href="/zen" class="btn btn-ghost btn-sm rounded-btn" use:link>
+				Zen
+			</a>
+			<a href="/dadjokes" class="btn btn-ghost btn-sm rounded-btn" use:link>
+				Dad Jokes
+			</a>
+			<a href="/blog" class="btn btn-ghost btn-sm rounded-btn" use:link>
+				Blog
+			</a>
+		</div>
+	</div> 
+	<div class="navbar-end">
+		<button class="btn btn-square btn-ghost md:hidden" on:click={toggleMenu}>
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">           
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>               
+      </svg>
+		</button>
+	</div>
+</div>
