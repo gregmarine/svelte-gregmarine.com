7eb6784c1461e428710aa7fdb100e6fdea37ead5
Greg Marine <gregmarine@hey.com>
2021-10-28 20:19:03 -0400

    Updated the Blog component to implement the new style

--- src/routes/Blog.svelte
+++ src/routes/Blog.svelte
@@ -44,13 +44,7 @@
 </script>
 
 {#if id === ""}
-  <div class="container mx-auto flex flex-wrap pt-24" in:fade|local>
-    <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center">
-      Blog
-    </h1>
-  </div>
-
-  <div class="container mx-auto flex flex-wrap pt-4 pb-12" in:fade|local>
+  <div class="container mx-auto flex flex-wrap pt-24 pb-12" in:fade|local>
     <div class="w-full mb-4">
       <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t" />
     </div>
@@ -62,45 +56,25 @@
     </h2>
 
     {#each $blog as doc}
-      <article
-        class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink"
-        in:fade|local
-      >
-        <div
-          class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow"
-        >
-          <a
-            href="/blog/{doc.id}"
-            class="flex flex-wrap no-underline hover:no-underline"
-            use:link
-          >
-            <p class="w-full text-gray-600 text-l md:text-xl px-6 pt-6">
-              {doc.title}
-            </p>
-          </a>
-
-          <a
-            href="/blog/{doc.id}"
-            class="flex flex-wrap no-underline hover:no-underline"
-            use:link
-          >
-            <p class="w-full text-gray-600 text-xs md:text-sm px-6 py-6">
-              {doc.text}
-            </p>
-          </a>
+      <div class="flex md:w-1/2 lg:w-1/3 xl:w-1/4 p-2" in:fade|local>
+        <div class="card bordered shadow-lg">
+          <figure class="px-10 pt-10">
+            <img class="object-cover h-96 md:h-48 w-full rounded-lg" alt={doc.title} src="/collections/blog/{doc.id}/image.webp" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">{doc.title}</h2>
+            <p class="sm:text-sm md:text-xs">{doc.text}</p>
+            <div class="card-actions">
+              <a class="btn btn-primary" href="/blog/{doc.id}" use:link>Read More</a>
+            </div>
+          </div>
         </div>
-      </article>
+      </div>
     {/each}
   </div>
 {:else}
   {#if doc}
-    <div class="container mx-auto flex flex-wrap pt-24" in:fade|local>
-      <h1 class="w-full my-2 text-3xl font-bold leading-tight text-center">
-        {doc.title}
-      </h1>
-    </div>
-
-    <div class="w-full flex flex-col justify-center items-center">
+    <div class="w-full flex flex-col justify-center items-center pt-24">
       <div class="flex flex-wrap w-full xl:w-1/2 md:w-4/6 sm:w-5/6" in:fade>
         <p class="w-full p-6 space-y-6">
           {doc.text}
